c0eaba6f9f002ccf3a8340061bd8eb8a06e4b217
Ildikó Szabó <szab.ildiko89@gmail.com>
2023-09-27 20:42:48 +0200

    first card

--- src/App.js
+++ src/App.js
@@ -2,32 +2,39 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
-  const [cards, setCards] = useState("");
-  
+  const [pokemonData, setPokemonData] = useState({});
+
   useEffect(() => {
-  const url = "https://pokeapi.co/api/v2/pokemon/ditto";    
-  
-  const fetchData = async () => { 
-    try { 
-      const response = await fetch(url)
-      const json = await response.json();
-      setCards(json.sprites.other["official-artwork"].front_default);
-    } catch (error) { 
-      console.log("error", error);
-    }
-  }; 
+    const url = 'https://pokeapi.co/api/v2/pokemon/1';
 
-  fetchData();
-}, []); 
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setPokemonData({
+          image: json.sprites.other['official-artwork'].front_default,
+          id: json.id,
+          name: json.name,
+          types: json.types.map(typeEntry => typeEntry.type.name),
+        });
+      } catch (error) {
+        console.log('error', error);
+      }
+    };
 
+    fetchData();
+  }, []);
 
   return (
     <div>
-      <img src={cards} alt="ditto" className="images"></img>
+      <img src={pokemonData.image} alt='pokemonImage' className='images'></img>
+      <p>#{pokemonData.id}</p>
+      <p>{pokemonData.name}</p>
+      {pokemonData.types && pokemonData.types.map((type, i) => {
+        return <div key={i}>{type}</div>;
+      })}
     </div>
   );
 };
-
-
 
 export default App;
