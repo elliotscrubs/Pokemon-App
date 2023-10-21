1cc8ffa24d43616a402c33d158e8cb7bcb7cfbce
Ildikó Szabó <szab.ildiko89@gmail.com>
2023-10-21 13:05:17 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,50 +1,25 @@
-import React, {useState} from "react";
-import './Card.css';
-import Card from './Card';
+import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
+import CardsView from './CardsView';
+import './Card.css';
 
 const App = () => {
-  const [idList, setIdList] = useState(() => {
-    var array  = [];
-    
-    for (var i = 1; i <= 12; i++) {
-      array.push(i);      
-    }
-    return array;
-  });
-  
-
-  const loadMore = () => { 
-    var array = [...idList]
-
-    for (var i = idList.length + 1; i <= idList.length + 12; i++) {
-      array.push(i);      
-    };
-    setIdList(array)
-  }
-
   return (
     <>
-      <div className="search">
+      <div className='search'>
         <h1>Pokemon Search</h1>
         <div className='search'>
           <TextField
             id='outlined-basic'
             variant='outlined'
-            width= '50%'
+            width='50%'
             label='Pokemon name'
           />
         </div>
       </div>
-      <div className='container'>
-        {idList.map(id => {
-          return <Card id={id}>{id}</Card>;
-        })}
-      </div>
-      <button onClick={loadMore} className="loadButton"> Load More </button>
+      <CardsView />
     </>
   );
 };
 
 export default App;
-
---
+++ src/CardsView.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import Card from './Card';
+import './App.css';
+
+function CardsView() {
+  const [idList, setIdList] = useState(() => {
+    var array = [];
+
+    for (var i = 1; i <= 12; i++) {
+      array.push(i);
+    }
+    return array;
+  });
+
+  const loadMore = () => {
+    var array = [...idList];
+
+    for (var i = idList.length + 1; i <= idList.length + 12; i++) {
+      array.push(i);
+    }
+    setIdList(array);
+  };
+
+  return (
+    <>
+      <div className='container'>
+        {idList.map(id => {
+          return <Card id={id}>{id}</Card>;
+        })}
+      </div>
+      <button onClick={loadMore} className='loadButton'>
+        {' '}
+        Load More{' '}
+      </button>
+    </>
+  );
+}
+
+export default CardsView;
