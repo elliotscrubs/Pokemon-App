c8e1fc791aa7830db15ad7cc129497bd3fea0b8b
Ildikó Szabó <szab.ildiko89@gmail.com>
2023-11-03 10:20:26 +0100

    done

--- src/DetailView.js
+++ src/DetailView.js
@@ -23,12 +23,15 @@ function DetailView(props) {
           ),
           height: jsonPokemon.height,
           weight: jsonPokemon.weight,
-          habitat: jsonSpecies.habitat.name,
-          flavorText: jsonSpecies.flavor_text_entries
-            .filter(flavor_text => flavor_text.language.name === 'en' ? flavor_text : '')
-              .map(text => text.flavor_text.replace(/\u000c/g,
-              ' '))
-        })
+          habitat: jsonSpecies.habitat?.name,
+          flavorText: removeDuplicates(
+            jsonSpecies.flavor_text_entries
+              .filter(flavor_text =>
+                flavor_text.language.name === 'en' ? flavor_text : ''
+              )
+              .map(text => text.flavor_text.replace(/\u000c/g, ' '))
+              ),
+        });
       } catch (error) {
         console.log('error', error);
       }
@@ -36,6 +39,18 @@ function DetailView(props) {
 
     fetchData();
   }, [props.pokemonsUrl]);
+
+  function removeDuplicates(flavor_text) {
+    return flavor_text.reduce(
+      (acc, curr) =>
+        acc
+          .map(str => str.toLowerCase().replaceAll(/\s/g, ''))
+          .includes(curr.toLowerCase().replaceAll(/\s/g, ''))
+          ? acc
+          : [...acc, curr],
+      []
+    );
+  }
 
   return (
     <>
@@ -52,41 +67,58 @@ function DetailView(props) {
         <div className='detailsContainer'>
           <p className='detailsPokeId'>#{pokemonData.id}</p>
           <p className='detailsPokeName'>{pokemonData.name}</p>
-          <p>
-            <span style={{ color: 'green' }}> Type: </span>
-          </p>
-          <ul>
+          <div className='table'>
+            <p>
+              <span style={{ color: 'green', fontWeight: 'bold' }}>
+                {' '}
+                Type:{' '}
+              </span>
+            </p>
             {pokemonData.types &&
               pokemonData.types.map((type, i) => {
-                return <li key={i}>{type}</li>;
+                return <p key={i}> {type} </p>;
               })}
-          </ul>
-          <p>
-            <span style={{ color: 'green' }}>Abilities:</span>
-          </p>
-          <ul>
+            <p>
+              <span style={{ color: 'green', fontWeight: 'bold' }}>
+                Abilities:
+              </span>
+            </p>
             {pokemonData.abilities &&
               pokemonData.abilities.map((ability, i) => {
                 return (
-                  <li className='pokeMove' key={i}>
+                  <p className='pokeMove' key={i}>
                     {ability}
-                  </li>
+                  </p>
                 );
               })}
-          </ul>
+            <p>
+              <span style={{ color: 'green', fontWeight: 'bold' }}>
+                Height:{' '}
+              </span>
+              {pokemonData.height / 10} m
+            </p>
+            <p>
+              <span style={{ color: 'green', fontWeight: 'bold' }}>
+                Weight:{' '}
+              </span>
+              {pokemonData.weight / 10} kg
+            </p>
+            <p>
+              <span style={{ color: 'green', fontWeight: 'bold' }}>
+                Habitat:{' '}
+              </span>
+              {pokemonData.habitat}
+            </p>
+          </div>
           <p>
-            <span style={{ color: 'green' }}>Height: </span>
-            {pokemonData.height / 10} m
+            <span style={{ color: 'green', fontWeight: 'bold'  }}>
+              Descriptions:
+            </span>
+            {pokemonData.flavorText &&
+              pokemonData.flavorText.map((text, i) => {
+                return <p key={i}> {text} </p>;
+              })}
           </p>
-          <p>
-            <span style={{ color: 'green' }}>Weight: </span>
-            {pokemonData.weight / 10} kg
-          </p>
-          <p>
-            <span style={{ color: 'green' }}>Habitat: </span>
-            {pokemonData.habitat}
-          </p>
-          <p className='description'> {pokemonData.flavorText} </p>
         </div>
       </div>
     </>
